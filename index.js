804bde55085d5807b1105cdb46c27dcf518b1000
programmerrabbanii <developerrabbanisarker@gmail.com>
2024-12-13 12:20:22 +0600

    specific id on details page

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors=require('cors')
 require('dotenv').config()
 const app=express()
@@ -31,6 +31,13 @@ async function run() {
         const result=await getJobs.toArray()
         res.send(result) 
     })
+    app.get('/jobs/:id', async (req,res)=>{
+        const id=req.params.id;
+        const query={_id: new ObjectId(id)}
+        const result=await jobsCollection.findOne(query)
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
